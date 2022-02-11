100151e9b23b6aefecfb1448d31c666f66c8a3df
alexandr.fedorenko <alexandr.fedorenko@onix-systems.com>
2022-02-11 18:02:48 +0200

    3
     - optimize mobile layout

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5007b09047fe04cd6ff6f5219753e366",
-"index.html": "36528de4c2962b1a2fdde37746c207e7",
-"/": "36528de4c2962b1a2fdde37746c207e7",
-"main.dart.js": "64ea9bee3fbac7077a99a166b3f5f661",
+"index.html": "04451016812b2f7c267902c64c03566a",
+"/": "04451016812b2f7c267902c64c03566a",
+"main.dart.js": "4afcf8e1284508360b4f9d6ca262cf57",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
